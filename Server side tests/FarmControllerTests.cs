be2cdfaafcb14dc9fe5d5f051cf7177d111990b7
Alexey <lmokhareva@gmail.com>
2023-07-17 11:54:45 +0300

    Finish farm test controller

--- Server side tests/FarmControllerTests.cs
+++ Server side tests/FarmControllerTests.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using InnoGotchi_backend.Controllers;
 using InnoGotchi_backend.Models.Dto;
 using InnoGotchi_backend.Models.Entity;
@@ -6,13 +7,14 @@ using InnoGotchi_backend.Services;
 using InnoGotchi_backend.Services.Abstract;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Any;
 using Moq;
 using System.Security.Claims;
 using System.Text.Json;
 
 namespace MoqTests
 {
-    public class FarmServiceTests
+    public class FarmControllerTests
     {
         [Fact]
         public async Task TestCreateNewFarm()
@@ -35,7 +37,7 @@ namespace MoqTests
                 { HttpContext = new DefaultHttpContext { User = userMock.Object } }
             };
 
-            var result = await controller.CreateFarm(dto) as ObjectResult;
+            ObjectResult? result = await controller.CreateFarm(dto) as ObjectResult;
 
             //Assert
             Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
@@ -60,14 +62,50 @@ namespace MoqTests
             //Act
             FarmsController controller = new FarmsController(farmServiceMoke.Object, null, null, null)
             {
-                ControllerContext = new Microsoft.AspNetCore.Mvc.ControllerContext
+                ControllerContext = new ControllerContext
                 { HttpContext = new DefaultHttpContext { User = userMock.Object } }
             };
 
-            var result = await controller.CreateFarm(dto) as ObjectResult;
+            ObjectResult? result = await controller.CreateFarm(dto) as ObjectResult;
 
             //Assert
             Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+
+        }
+
+        [Fact]
+        public async Task GetCurrentFarmTest()
+        {
+            //Arrange
+            var mockFarmService = new Mock<IFarmService>();
+            var userMock = new Mock<ClaimsPrincipal>();
+            var mockMapper = new Mock<IMapper>();
+
+            Farm farm = new Farm()
+            {
+                FarmName = "my favourite farm",
+                AlivePetsCount = 5,
+                DeadPetsCount = 8
+            };
+            mockMapper.Setup(x => x.Map<FarmDto>(farm))
+                .Returns(new FarmDto { FarmName = "my favourite farm", AlivePetsCount = 5, DeadPetsCount = 8 });
+            
+            mockFarmService.Setup(x => x.GetFarm("q")).ReturnsAsync(farm);
+            
+            userMock.Setup(x => x.FindFirst(ClaimTypes.Email))
+            .Returns(new Claim(ClaimTypes.Email, "q"));
+
+            //Act
+            FarmsController controller = new FarmsController(mockFarmService.Object, mockMapper.Object, null, null)
+            {
+                ControllerContext = new ControllerContext
+                { HttpContext = new DefaultHttpContext { User = userMock.Object } }
+            };
+
+            var result = await controller.GetCurrentFarm() as ObjectResult;
+
+            //Assert
+            Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
 
         }
 
